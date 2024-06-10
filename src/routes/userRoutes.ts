11170490e5ee05cc9633d7f637cae3a7ec03f116
Pedro Henrique <pedroballack1167@gmail.com>
2024-06-10 05:21:18 -0300

    refactor: middleware in delete user route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { authenticateToken } from "../middlewares/authenticateToken"
 import * as userController from "../controllers/userControlers"
 
 const userRoutes = Router()
@@ -6,6 +7,6 @@ const userRoutes = Router()
 userRoutes.get('/user', userController.getAllUsers)
 userRoutes.post('/login', userController.loginUser)
 userRoutes.post('/register', userController.createNewUser)
-userRoutes.delete('/user/:id', userController.deleteUser)
+userRoutes.delete('/user/:id', authenticateToken, userController.deleteUser)
 
 export default userRoutes
