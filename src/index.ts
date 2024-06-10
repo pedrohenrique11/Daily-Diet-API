1e6f01a5a17177479db41fa6e0b3066bb6afa6ef
Pedro Henrique <pedroballack1167@gmail.com>
2024-06-10 05:21:51 -0300

    build: add jsonwebtoken dependence

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from "express"
 import userRoutes from "./routes/userRoutes"
+import dotenv from "dotenv"
+
+import { authenticateToken } from "./middlewares/authenticateToken"
 
 const app = express()
+dotenv.config()
 
 app.use(express.json())
 app.use(userRoutes)
 
-app.listen(3333, () => {
+app.get('/teste', authenticateToken, () => {
+    console.log("tome tome")
+})
+
+app.listen(process.env.PORT, () => {
     console.log('server is running...')
 })
