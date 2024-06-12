3fa6ea5e5639342ac9c4f5d5c09028413c643c24
Pedro Henrique <pedroballack1167@gmail.com>
2024-06-12 10:34:50 -0300

    feat: add user update route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,6 +7,7 @@ const userRoutes = Router()
 userRoutes.get('/user', userController.getAllUsers)
 userRoutes.post('/login', userController.loginUser)
 userRoutes.post('/register', userController.createNewUser)
+userRoutes.put('/edit/:id', authenticateToken,userController.updateUser)
 userRoutes.delete('/user/:id', authenticateToken, userController.deleteUser)
 
 export default userRoutes
