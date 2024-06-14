34442b145264762aac5bda3765502b39394002bf
Pedro Henrique <pedroballack1167@gmail.com>
2024-06-14 05:04:38 -0300

    feat: add meal routes to index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import express from "express"
 import userRoutes from "./routes/userRoutes"
+import mealRoutes from "./routes/mealRoutes"
 import dotenv from "dotenv"
-
-import { authenticateToken } from "./middlewares/authenticateToken"
 
 const app = express()
 dotenv.config()
 
 app.use(express.json())
 app.use(userRoutes)
-
-app.get('/teste', authenticateToken, () => {
-    console.log("tome tome")
-})
+app.use(mealRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log('server is running...')
