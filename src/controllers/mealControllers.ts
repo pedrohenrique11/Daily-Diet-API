7b824d09ac861a24a84ea4ad7b2bea258bf587d0
Pedro Henrique <pedroballack1167@gmail.com>
2024-06-14 05:45:20 -0300

    feat: add uptade and delete meal functions

--- src/controllers/mealControllers.ts
+++ src/controllers/mealControllers.ts
@@ -6,7 +6,7 @@ import { stringify } from "querystring";
 export const getAllmeals = async (req:Request, res: Response) => {
     const { userId } = req.user
     const meals = await mealsModel.getAllmeals(userId)
-    
+
     res.status(201).json(meals)
 }
 
@@ -14,7 +14,26 @@ export const createMeal = async (req: Request, res:Response) => {
     const { title, description, onDiet } = req.body
     const { userId } = req.user
 
-    const meal = await mealsModel.createMeal(title, description, onDiet, userId)
+    await mealsModel.createMeal(title, description, onDiet, userId)
 
     res.status(201).send("added meal!")
+}
+
+export const updateMeal = async (req: Request, res: Response) => {
+    const { title, description, onDiet } = req.body
+    const id = parseInt(req.params.id)
+    const { userId } = req.user
+
+    await mealsModel.updateMeal(id, title, description, onDiet, userId)
+
+    res.status(204).send("Meal updated")
+}
+
+export const deleteMeal = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    const { userId } = req.user
+
+    await mealsModel.deleteMeal(id,  userId)
+
+    res.status(204).send("Meal deleted")
 }
